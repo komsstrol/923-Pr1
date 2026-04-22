3d024df7058e11985713ad9988a93cc9655f87d9
Ruslan <trolkoms483@gmail.com>
2026-04-22 23:27:50 +0600

    16: Добавлены внутренние страницы с переменными head, header, footer, content

---
+++ home/php/public/Задание 15/inc/template-inside.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="ru">
+<head>
+    <?php echo $head; ?>
+</head>
+<body>
+    <header>
+        <div class="content ins">
+            <?php echo $header; ?>
+        </div>
+    </header>
+
+    <div class="main ins">
+        <div class="content ins">
+            <?php echo $content; ?>
+        </div>
+    </div>
+
+    <footer>
+        <div class="content">
+            <?php echo $footer; ?>
+        </div>
+    </footer>
+</body>
+</html>
